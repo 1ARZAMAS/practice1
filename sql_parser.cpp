4406d579a4554f680ca52b05cb28d67f4aa0aa30
1ARZAMAS <lizaakutkina@gmail.com>
2024-10-27 20:13:17 +0700

    added test if table exists

--- sql_parser.cpp
+++ sql_parser.cpp
@@ -7,6 +7,55 @@
 
 using namespace std;
 
+bool columnExists(const LinkedList& columnsFromQuery, const std::string& columnName) {
+    
+    return false; // Колонка не найдена
+}
+
+
+void splitPoint(LinkedList& tablesFromQuery, LinkedList& columnsFromQuery, std::string& wordFromQuery, const DatabaseManager& dbManager) {
+    // Удаляем пробелы в начале и конце
+    wordFromQuery.erase(0, wordFromQuery.find_first_not_of(" "));
+    wordFromQuery.erase(wordFromQuery.find_last_not_of(" ") + 1);
+
+    // Найдем позицию точки
+    size_t dotPos = wordFromQuery.find('.');
+    if (dotPos != std::string::npos) {
+        tablesFromQuery.addToTheHead(wordFromQuery.substr(0, dotPos)); // Сохраняем имя таблицы
+        columnsFromQuery.addToTheHead(wordFromQuery.substr(dotPos + 1)); // Сохраняем имя колонки
+    } else {
+        std::cout << "Incorrect format: " << wordFromQuery << std::endl;
+        return;
+    }
+
+    // Проверяем существование таблицы
+    UniversalNode* current = dbManager.tables.head;
+    bool tableExists = false;
+    DBtable* currentTablePtr = nullptr;
+
+    while (current != nullptr) {
+        DBtable& currentTable = reinterpret_cast<DBtable&>(current->data); // Приведение к типу DBtable
+        if (currentTable.tableName == tablesFromQuery.head->data) { // Сравниваем имя таблицы
+            tableExists = true;
+            currentTablePtr = &currentTable; // Сохраняем указатель на найденную таблицу
+            break; // Таблица найдена
+        }
+        current = current->next; // Переход к следующему узлу
+    }
+
+    if (!tableExists) {
+        std::cerr << "There is no such table" << std::endl;
+        return;
+    }
+
+    // bool columnExists = false;
+    // НУЖНО ДОБАВИТЬ ПРОВЕРКУ НА СУЩЕСТВОВАНИЕ КОЛОНОК!
+    // if (!columnExists) {
+    //     std::cerr << "There is no such column" << std::endl;
+    //     return;
+    // }
+}
+
 void QueryManager(const DatabaseManager& dbManager) {
     string command;
     while(true){
@@ -20,25 +69,32 @@ void QueryManager(const DatabaseManager& dbManager) {
             return;
         } else if (wordFromQuery == "SELECT"){
             try {
-                cout << "sel" << endl;
+                LinkedList tablesFromQuery;
+                LinkedList columnsFromQuery;
+                iss >> wordFromQuery; // таблица1.колонка1
+                splitPoint(tablesFromQuery, columnsFromQuery, wordFromQuery, dbManager);
+
+                iss >> wordFromQuery; // таблица2.колонка1
+                splitPoint(tablesFromQuery, columnsFromQuery, wordFromQuery, dbManager);
+                
 
             } catch (const exception& ErrorInfo) {
                 cerr << ErrorInfo.what() << endl;
             }
-        } else if (wordFromQuery == "DELETE"){
-            try {
-                cout << "del" << endl; 
+        // } else if (wordFromQuery == "DELETE"){
+        //     try {
+        //         cout << "del" << endl; 
 
-            } catch (const exception& ErrorInfo) {
-                cerr << ErrorInfo.what() << endl;
-            }
-        } else if (wordFromQuery == "INSERT"){
-            try {
-                cout << "ins" << endl;
+        //     } catch (const exception& ErrorInfo) {
+        //         cerr << ErrorInfo.what() << endl;
+        //     }
+        // } else if (wordFromQuery == "INSERT"){
+        //     try {
+        //         cout << "ins" << endl;
 
-            } catch (const exception& ErrorInfo) {
-                cerr << ErrorInfo.what() << endl;
-            }
+        //     } catch (const exception& ErrorInfo) {
+        //         cerr << ErrorInfo.what() << endl;
+        //     }
         } else {
             cerr << "Incorrect SQL query" << endl;
         }
