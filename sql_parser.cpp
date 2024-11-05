ce17e106b6c75737a8ee4a4a43a26169217ae0ce
1ARZAMAS <lizaakutkina@gmail.com>
2024-11-05 23:35:44 +0700

    added select but cannot test it bc there is no insert func

--- sql_parser.cpp
+++ sql_parser.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <sstream>
 #include "header.h"
+#include "rapidcsv.hpp"
 
 using namespace std;
 namespace fs = std::filesystem;
@@ -71,16 +72,38 @@ int amountOfCSV(const DatabaseManager& dbManager, const std::string& tableName) 
     return amount; // возвращаем количество найденных файлов
 }
 
-
-void crossJoin(int& fileCountFirstTable, int& fileCountSecondTable, const DatabaseManager& dbManager, const std::string& tableName){
+void crossJoin(int& fileCountFirstTable, int& fileCountSecondTable, const DatabaseManager& dbManager, const std::string& tableName, LinkedList& columnsFromQuery){
     for (int i = 1; i < fileCountFirstTable; i++){ // пройдемся по всем файлам первой таблицы
-        string tableDir1 = dbManager.schemaName + "/" + tableName + "/" + tableName + "_" + std::to_string(i + 1) + ".csv";
-        // для правильности надо обрезать будет, потому что будет работать только для первой таблицы, а не для второй
-        // либо можно попробовать не обрезать, а использовать некст значение в списке, чтобы прийти ко второму записанному (звучит хорошо)
-        
-        for (int j = 1; j < fileCountSecondTable; j++){ // пройдемся по второй таблице
+        //UniversalNode* current = dbManager.tables.head; // заходим в дб менеджер, где хранятся все названия
+        DBtable& firstTable = reinterpret_cast<DBtable&>(dbManager.tables.head->data); // Access the first table
+        string currentTable1 = firstTable.tableName; // Extract table name as string
 
+        string tableDir1 = dbManager.schemaName + "/" + currentTable1 + "/" + currentTable1 + "_" + std::to_string(i + 1) + ".csv";
+        //current = current->next; // переходим к следующему узлу
+        string column1 = columnsFromQuery.head->data;
+        
+        rapidcsv::Document document1(tableDir1); // открываем файл 1
+        int indexFirstColumn = document1.GetColumnIdx(column1); // считываем индекс искомой колонки 1
+        int amountRow1 = document1.GetRowCount(); // считываем количество строк в файле 1
+        for (int j = 0; j < amountRow1; j++){ // проходимся по всем строкам
+            for (int k = 1; k < fileCountSecondTable; k++){ // пройдемся по второй таблице
+                DBtable& secondTable = reinterpret_cast<DBtable&>(dbManager.tables.head->next->data); // Access the second table
+                string currentTable2 = secondTable.tableName; // Extract table name as string
+                
+                string tableDir2 = dbManager.schemaName + "/" + currentTable2 + "/" + currentTable2 + "_" + std::to_string(i + 1) + ".csv";
+                string column2 = columnsFromQuery.head->next->data;
+                rapidcsv::Document document2(tableDir2); // открываем файл 2
+                int indexSecondColumn = document2.GetColumnIdx(column1); // считываем индекс искомой колонки 2
+                int amountRow2 = document2.GetRowCount(); // считываем количество строк в файле 2
+                for (size_t p = 0; p < amountRow2; ++p) {
+                    cout << document1.GetCell<string>(0, j) << ": ";
+                    cout << document1.GetCell<string>(indexFirstColumn, j) << "  |   ";
+                    cout << document2.GetCell<string>(0, p) << ": ";
+                    cout << document2.GetCell<string>(indexSecondColumn, p) << endl;
+                }
+            }
         }
+        
     }
 }
 
@@ -107,7 +130,7 @@ void QueryManager(const DatabaseManager& dbManager, DBtable& table) {
                 splitPoint(tablesFromQuery, columnsFromQuery, wordFromQuery, dbManager);
                 int fileCountSecondTable = amountOfCSV(dbManager, tablesFromQuery.head->data);
 
-                crossJoin(fileCountFirstTable, fileCountSecondTable, dbManager, tablesFromQuery.head->data);
+                crossJoin(fileCountFirstTable, fileCountSecondTable, dbManager, tablesFromQuery.head->data, columnsFromQuery);
 
             } catch (const exception& ErrorInfo) {
                 cerr << ErrorInfo.what() << endl;
