37fcfd4acf5be3d3d11776903c062a39cd830799
1ARZAMAS <lizaakutkina@gmail.com>
2024-11-10 01:58:12 +0700

    trying to do select with where

--- sql_parser.cpp
+++ sql_parser.cpp
@@ -29,20 +29,79 @@ void crossJoin(int& fileCountFirstTable, int& fileCountSecondTable, const Databa
                 int indexSecondColumn = document2.GetColumnIdx(column2); // считываем индекс искомой колонки 2
                 int amountRow2 = document2.GetRowCount(); // считываем количество строк в файле 2
                 for (int p = 0; p < amountRow2; ++p) {
-                    // if (indexFirstColumn < document1.GetColumnCount()) {
-                    //     cout << document1.GetCell<string>(indexFirstColumn, j) << "  |   ";
-                    // } else {
-                    //     cerr << "Column index out of bounds for first table" << endl;
-                    // }
-                    // if (indexSecondColumn < document2.GetColumnCount()) {
-                    //     cout << document2.GetCell<string>(indexSecondColumn, p) << endl;
-                    // } else {
-                    //     cerr << "Column index out of bounds for second table" << endl;
-                    // }
                     cout << document1.GetCell<string>(0, j) << ": ";
                     cout << document1.GetCell<string>(indexFirstColumn, j) << "  |   ";
                     cout << document2.GetCell<string>(0, p) << ": ";
                     cout << document2.GetCell<string>(indexSecondColumn, p) << endl;
+                }
+            }
+        }
+    }
+}
+
+bool recursionFunc(string query){
+    int pos_or = query.find("OR"); // ищем первое вхождение OR
+    if (pos_or != string::npos) {
+        string leftPart = query.substr(0, pos_or);  // отсекаем левую часть до OR
+        string rightPart = query.substr(pos_or + 2);  // отсекаем левую часть после OR
+        bool leftResult = recursionFunc(cleanString(leftPart));
+        bool rightResult = recursionFunc(cleanString(rightPart));
+
+        return leftResult || rightResult;  // если хотя бы одно истинно, вернем true
+    }
+    int pos_and = query.find("AND"); // ищем первое вхождение AND
+    if (pos_and != string::npos) {
+        string leftPart = query.substr(0, pos_and);  // отсекаем левую часть до AND
+        string rightPart = query.substr(pos_and + 3);  // отсекаем левую часть после AND
+        bool leftResult = recursionFunc(cleanString(leftPart));
+        bool rightResult = recursionFunc(cleanString(rightPart));
+
+        return leftResult && rightResult;  // если оба истинно, вернем true
+    }
+    int pos_equal = query.find('=');
+    if (pos_equal != string::npos){
+        string leftPart = cleanString(query.substr(0, pos_equal));  // левая часть до =
+        string rightPart = cleanString(query.substr(pos_equal + 1));  // правая часть после =
+
+    }
+    return false; // если нет =, значит, условия точного нет и ничего выводить не будем
+}
+
+// SELECT table1.column1, table2.column1 FROM table1, table2 WHERE table1.column1 = table2.column2 AND table1.column2 = 'string'
+// tablesFromQuery table1 table2
+// columnsFromQuery column1 column1
+void selectWithWhere(int& fileCountFirstTable, int& fileCountSecondTable, const DatabaseManager& dbManager, const std::string& query, LinkedList& tablesFromQuery, LinkedList& columnsFromQuery) {
+    // if (!findDot(/**/)){ // Если не нашли точку в выражении, значит, что это просто строка, с которой будем сравнивать значение
+    //     cleanString(/**/); // а для этого почистим строку от лишних символов
+    // }
+
+    for (int i = 0; i < fileCountFirstTable; i++){ // пройдемся по всем файлам первой таблицы
+        DBtable& firstTable = reinterpret_cast<DBtable&>(dbManager.tables.head->data); // приводим к типу DBtable
+        string currentTable1 = firstTable.tableName; // получаем имя таблицы
+        string tableDir1 = dbManager.schemaName + "/" + currentTable1 + "/" + currentTable1 + "_" + std::to_string(i + 1) + ".csv";
+        //string column1 = cleanString(columnsFromQuery.head->next->data);
+        rapidcsv::Document document1(tableDir1); // открываем файл 1
+        //int indexFirstColumn = document1.GetColumnIdx(column1); // считываем индекс искомой колонки 1
+        for (int j = 0; j < document1.GetRowCount(); j++) {
+            for (int k = 0; k < fileCountSecondTable; k++){ // пройдемся по второй таблице
+                DBtable& secondTable = reinterpret_cast<DBtable&>(dbManager.tables.head->next->data); // приводим к типу DBtable
+                string currentTable2 = secondTable.tableName; // получаем имя таблицы
+                string tableDir2 = dbManager.schemaName + "/" + currentTable2 + "/" + currentTable2 + "_" + std::to_string(k + 1) + ".csv";
+                //string column2 = cleanString(columnsFromQuery.head->data);
+                rapidcsv::Document document2(tableDir2); // открываем файл 2
+                //int indexSecondColumn = document2.GetColumnIdx(column2); // считываем индекс искомой колонки 2
+                
+                for (int p = 0; p < document2.GetRowCount(); ++p) {
+                    // Применение условий фильтрации
+                    if (recursionFunc(query, tablesFromQuery, columnsFromQuery)) {
+                        // Если условие выполнено, выводим соответствующие данные
+                        for (int p = 0; p < document2.GetRowCount(); ++p) {
+                            // cout << document1.GetCell<string>(0, j) << ": ";
+                            // cout << document1.GetCell<string>(indexFirstColumn, j) << "  |   ";
+                            // cout << document2.GetCell<string>(0, p) << ": ";
+                            // cout << document2.GetCell<string>(indexSecondColumn, p) << endl;
+                        }
+                    }
                 }
             }
         }
@@ -120,7 +179,7 @@ void QueryManager(const DatabaseManager& dbManager, DBtable& table) {
                     string valuesPart;
                     getline(iss, valuesPart); // считываем оставшуюся часть строки
                     query += valuesPart; // table1.column1 = table2.column2 AND table1.column2 = '123'
-                    
+                    selectWithWhere(fileCountFirstTable, fileCountSecondTable, dbManager, query);
                 } else {
                     crossJoin(fileCountFirstTable, fileCountSecondTable, dbManager, tablesFromQuery.head->data, columnsFromQuery);
                 }
